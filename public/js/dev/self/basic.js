a347443ba9b46f57f2bdbe92cd2e05a81da2b8c1
mrhop <233402923@qq.com>
2018-06-27 14:23:29 +0800

    add js lib

--- public/js/dev/self/basic.js
+++ public/js/dev/self/basic.js
@@ -2,8 +2,8 @@
  * Created by Donghui Huo on 2016/3/15.
  */
 var $ = jQuery = require('jquery');
-require("flexslider");
 require("bootstrap");
+var Swiper = require("Swiper");
 var utilFun = require("utilFun");
 $(document).ready(function () {
     $(".navbar-nav li.active").removeClass("active");
@@ -12,17 +12,14 @@ $(document).ready(function () {
     $(".left-menu li." + $("body").attr("id")).addClass("active");
     //do something
     if ($("body").attr("id") === 'index') {
-        $('.first-container .flexslider').flexslider({
-            fadeFirstSlide: false,
-            controlNav: false,
-            directionNav: true,
-            animationSpeed: 1000,
-            slideshowSpeed: 4000,
-            slideshow: true,
-            animation: "slide",
-            prevText: "",
-            nextText: "",
-        });
+        new Swiper('.top-slider .swiper-container', {
+            // Optional parameters
+            loop: true,
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            }
+        })
     }
 
 });
