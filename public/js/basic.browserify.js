30f978adb0b33c41f1edff0bb34cff7376f193dc
mrhop <233402923@qq.com>
2018-07-09 11:22:41 +0800

    add js lib

--- public/js/basic.browserify.js
+++ public/js/basic.browserify.js
@@ -41,17 +41,19 @@ $(document).ready(function () {
     $(".navbar-nav li." + $("body").attr("id") + "-li-parent").addClass("active");
     $(".left-menu li." + $("body").attr("id")).addClass("active");
     //do something
-    if ($("body").attr("id") === 'index') {
-        new Swiper('.top-slider .swiper-container', {
-            // Optional parameters
-            loop: true,
-            navigation: {
-                nextEl: '.swiper-button-next',
-                prevEl: '.swiper-button-prev',
-            }
-        })
-    }
-
+    new Swiper('.top-slider .swiper-container', {
+        // Optional parameters
+        loop: true,
+        speed: 600,
+        autoplay: {
+            delay: 2000,
+            disableOnInteraction: false,
+        },
+        // navigation: {
+        //     nextEl: '.swiper-button-next',
+        //     prevEl: '.swiper-button-prev',
+        // }
+    })
 });
 
 
--- public/js/dev/self/basic.js
+++ public/js/dev/self/basic.js
@@ -11,16 +11,18 @@ $(document).ready(function () {
     $(".navbar-nav li." + $("body").attr("id") + "-li-parent").addClass("active");
     $(".left-menu li." + $("body").attr("id")).addClass("active");
     //do something
-    if ($("body").attr("id") === 'index') {
-        new Swiper('.top-slider .swiper-container', {
-            // Optional parameters
-            loop: true,
-            navigation: {
-                nextEl: '.swiper-button-next',
-                prevEl: '.swiper-button-prev',
-            }
-        })
-    }
-
+    new Swiper('.top-slider .swiper-container', {
+        // Optional parameters
+        loop: true,
+        speed: 600,
+        autoplay: {
+            delay: 2000,
+            disableOnInteraction: false,
+        },
+        // navigation: {
+        //     nextEl: '.swiper-button-next',
+        //     prevEl: '.swiper-button-prev',
+        // }
+    })
 });
 
